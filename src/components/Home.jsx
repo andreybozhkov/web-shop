453bea534afc2e57ae4915a278ce4d1474fff2d2
andreybozhkov <bozhkov.andrey@gmail.com>
2018-08-30 09:53:08 +0300

    UPDATE additional home page tweaks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,23 +38,37 @@ export default class Home extends Component {
                                 });
                     });
                 });
+        } else if (sessionStorage.authtoken) {
+            requester.get('appdata', 'products', 'kinvey')
+                .then(res1 => {
+                    let promisedMap = res1.map((prod) => {
+                        return requester.get('appdata', 'categories', 'kinvey', { '_id': prod.category} )
+                            .then((cat) => {
+                                prod.categoryName = cat[0].name;
+                                return prod;
+                            });
+                        })
+                    Promise.all(promisedMap)
+                        .then((values) => {
+                            this.setState({products: values});
+                        });
+            });
         }
     }
     
     render() {
-        console.log(this.state.products);
         return (
             <div className='home-grid-container'>
                 {this.state.products.map((product) => (
                         <div key={product._id} className="card mb-3">
                             <h3 className="card-header">{product.name}</h3>
                             <div className="card-body">
-                                <h5 className="card-title">{product.price} BGN</h5>
-                                <h6 className="card-subtitle text-muted">{product.categoryName}</h6>
+                                <h5 className="card-title">Price: {product.price} BGN</h5>
+                                <h6 className="card-subtitle text-muted">Category: {product.categoryName}</h6>
                             </div>
                             <img style={{height: '200px', width: '100%', display: 'block'}} src="https://upload.wikimedia.org/wikipedia/commons/8/85/Smiley.svg" alt="product" />
                             <div className="card-body">
-                                <p className="card-text">{product.description}</p>
+                                <p className="card-text">Description: {product.description}</p>
                             </div>
                         </div>
                     ))}
